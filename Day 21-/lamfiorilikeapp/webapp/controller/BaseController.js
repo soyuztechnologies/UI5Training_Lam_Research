0acb16b70294cbe15eda12ca8b230af7580bd3ee
soyuztechnologies <contact@soyuztechnologies.com>
2022-08-04 14:38:37 +0530

    last changes

--- Day 21-/lamfiorilikeapp/webapp/controller/BaseController.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function(Controller) {
-    'use strict';
-    return Controller.extend("lam.fin.ar.controller.BaseController",{
-        
-    });
-});
