32a5a8733fd3f14b54d90257ff7098b15aa88937
soyuztechnologies <contact@soyuztechnologies.com>
2022-08-12 12:39:26 +0530

    last changes

---
+++ Day 28/freestyle_lrp/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("lrp.freestyle.freestylelrp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
